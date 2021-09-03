f9519e550eab0ef9a96bd6d70463a0039db4ae6d
maeda <maeda.ibuki554@mail.kyutech.jp>
2021-09-03 16:15:59 +0900

    new commit

--- line-bot.py
+++ line-bot.py
@@ -50,10 +50,11 @@ def callback():
 
     return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
 def sendMessage(event, text):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
 
-@handler.add(MessageEvent, message=TextMessage)
+# @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = scraping(event)
     sendMessage(event, text)
